cc18c818907bb13c6817806345de9e4b62ad8ca5
deni <deniphoto@mai.ru>
2025-05-01 15:16:23 +0300

    fixed conditions and improved interface

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,6 @@ export default function Header() {
         <div className="bg-white rounded-lg p-[10px]">
           <p className="text-3xl">DevRank</p>
         </div>
-        <div className="bg-white rounded-lg p-[10px]">
-          <p className="text-3xl">ОЦЕНКА</p>
-        </div>
         <div className="bg-white rounded-lg p-[10px] flex gap-1">
           <p className="text-2xl">RU /</p>
           <p className="text-2xl">EN</p>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Skills() {
   const [skill, setSkill] = React.useState<string>("");
-  const [level, setLevel] = React.useState<string>("");
+  const [level, setLevel] = React.useState<string>("...");
 
   const handleSkills = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSkill(e.target.value);
@@ -10,31 +10,50 @@ export default function Skills() {
 
   const evaluateLevel = () => {
     const skillsList = skill.toLowerCase().split(" ");
-    if (skillsList.includes("react") && skillsList.includes("typescript")) {
+    if (
+      skillsList.includes("html") ||
+      skillsList.includes("css") ||
+      skillsList.includes("javascript") ||
+      skillsList.includes("js")
+    ) {
+      setLevel("Junior");
+    } else {
+      setLevel("...");
+    }
+    if (
+      skillsList.includes("react") ||
+      skillsList.includes("typescript") ||
+      skillsList.includes("ts") ||
+      skillsList.includes("redux")
+    ) {
       setLevel("Middle");
-    } else setLevel("Junior");
+    }
+    if (!skill.trim()) {
+      setLevel("...");
+    }
   };
 
   return (
     <div className="m-auto w-max">
-      <div className="bg-white h-[170px] rounded-xl p-[12px] w-lg text-center">
+      <div className="bg-white h-max rounded-xl p-[12px] w-lg text-center">
         <h1 className="text-2xl font-bold mb-[20px]">
-          Узнай свой уровень фронтендера
+          Проверь свой уровень во фронтенде
         </h1>
-        <div className="flex gap-3.5">
+        <div className="flex gap-3.5 mb-5">
           <input
             onChange={(e) => handleSkills(e)}
             className="border-1 rounded-md w-sm p-[10px]"
             type="text"
+            placeholder="Укажите ваш стек, например: js, react"
           />
           <button
             onClick={evaluateLevel}
-            className="border-1 p-[5px] px-[10px] cursor-pointer hover:bg-gray-400 rounded-md"
+            className="border-1 p-[5px] px-[10px] cursor-pointer hover:bg-gray-400 rounded-md transition-all active:scale-95 active:shadow-inner"
           >
             Узнать свой уровень
           </button>
         </div>
-        <h1 className="p-[25px]">{level}</h1>
+        <h1 className="border-1 p-2 ">{level}</h1>
       </div>
     </div>
   );
