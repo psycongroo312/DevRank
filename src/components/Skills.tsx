a43ae2f8d9269d6e8051ffc4c4d58a15a4b257c7
deni <deniphoto@mai.ru>
2025-05-01 17:48:13 +0300

    add media queries

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -34,21 +34,25 @@ export default function Skills() {
   };
 
   return (
-    <div className="m-auto w-max">
-      <div className="bg-white h-max rounded-xl p-[12px] w-lg text-center">
+    <div className="m-auto w-max ">
+      <div className="bg-white h-max rounded-xl p-[12px] text-center w-3xs sm:w-lg">
         <h1 className="text-2xl font-bold mb-[20px]">
           Проверь свой уровень во фронтенде
         </h1>
-        <div className="flex gap-3.5 mb-5">
+        <div className="flex flex-col sm:flex-row gap-3.5 mb-5">
           <input
             onChange={(e) => handleSkills(e)}
-            className="border-1 rounded-md w-sm p-[10px]"
+            className="border rounded-md w-full px-3 py-2 text-sm
+              sm:w-auto sm:flex-1"
             type="text"
             placeholder="Укажите ваш стек, например: js, react"
           />
           <button
             onClick={evaluateLevel}
-            className="border-1 p-[5px] px-[10px] cursor-pointer hover:bg-gray-400 rounded-md transition-all active:scale-95 active:shadow-inner"
+            className="border rounded-md px-4 py-2 cursor-pointer 
+              bg-gray-100 hover:bg-gray-200 active:bg-gray-300
+              transition-all active:scale-95 whitespace-nowrap
+              text-sm"
           >
             Узнать свой уровень
           </button>
