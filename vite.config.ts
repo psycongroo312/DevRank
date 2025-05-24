7f67170b5bf7faf4e868471aad092764ee403c20
deni <deniphoto@mai.ru>
2025-05-24 16:52:08 +0300

    fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
- base: '/https://github.com/psycongroo312/DevRank/',
+ base: 'https://github.com/psycongroo312/DevRank/',
   plugins: [react(), tailwindcss()],
 });
